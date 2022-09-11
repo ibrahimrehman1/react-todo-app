8ae1a7b35d600b48ebcdfac232cabf987e34b338
ibrahimrehman1 <ibrahimrehman0346@gmail.com>
2022-09-11 21:06:49 +0500

    Refactor and Format code. Migrated to FC

--- src/App.js
+++ src/App.js
@@ -1,94 +1,142 @@
-import React, {Component} from "react"
-import {TextField, ButtonGroup} from "@material-ui/core"
+import React, { useState } from "react";
 
+// MUI Components
+import TextField from "@mui/material/TextField";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
-export default class App extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            value: "",
-            arr: []
+// TODO Component
+export default function TODO() {
+  const [value, setValue] = useState("");
+  const [arr, setArr] = useState([]);
 
-        }
+  const addTask = () => {
+    if (!value) {
+      alert("Please Enter a Task!");
+      return;
     }
 
-    addTask = () =>{
-        if (this.state.value){
-            if (this.state.arr.length){
-                for (var i in this.state.arr){
-                    if (this.state.arr[i] === this.state.value){
-                        alert("This Task is Already Included!")
-                        return
-                    }
-                }
-            }
-            let current = [this.state.value];
-            this.setState({
-                arr: this.state.arr.concat(current),
-                value: ""
-            });
-        }
-
+    if (arr.includes(value)) {
+      alert("Task already added to list!");
+    } else {
+      setArr(arr.concat([value]));
+      setValue("");
     }
+  };
 
-    delete = (event) =>{
-        let txt = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[0].innerText.split("\n")[0];
-        let index = this.state.arr.indexOf(txt);
-        let newArr = this.state.arr;
-        delete newArr[index]
-        this.setState({...this.state, arr: newArr})
-    }
+  const delete_ = (index) => {
+    setArr(arr.filter((val, ind) => index !== ind));
+  };
 
-    edit = (event) => {
-        let value = prompt("Enter New Task: ");
-        for (var i in this.state.arr){
-            if (this.state.arr[i] === value){
-                alert("This Task is Already Included!")
-                return
-            }
-        }
-        if (value){
-            console.log(event.target)
-            let x = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[0].innerText.split("\n")[0];
-            console.log(`{${x}}`)
-            let newArr = this.state.arr.map((obj)=>{
-                if (obj === x){
-                    return value
-                }else{
-                    return obj
-                }
-            })
-            console.log(`NewArr: ${newArr}`)
-            this.setState({...this.state, arr: newArr})
-        }
-    }
+  const edit = (index) => {
+    let value = prompt("Enter New Task: ");
 
-    render(){
-        return(
-            <React.Fragment>
-                <h1 color="primary" style={{textAlign: "center", background: "#f50057", marginTop: "0px", fontSize: "2.5rem", padding: "0.1em"}}>TODO APP</h1>
-                <div className="main">
-                        <div style={{display: "inline-block"}}>
-                            <TextField id="standard-basic" label="Enter Task" onChange={(e)=>this.setState({value: e.target.value})} value={this.state.value}/>
-                            <button onClick={this.addTask} style={{backgroundColor: "#f50057", borderWidth: "0px",borderRadius: "3px", fontSize: "0.875rem", padding: "8px 18px", color: "white", verticalAlign: "bottom"}} className="btn">ADD</button>
-                            <ul style={{listStyleType: "square", listStylePosition: "inside", paddingLeft: "0px", textAlign: "start"}}>
-                                {this.state.arr.map((obj, index)=>{
-                                    return(
-                                        <li key={index} style={{marginBottom: "20px"}}>
-                                            <h4 style={{display: "inline-block"}}>{obj}</h4>
-                                            <div style={{display: "inline-block"}}>
-                                                <ButtonGroup style={{paddingLeft: "20px"}}>
-                                                    <button onClick={this.edit} style={{backgroundColor: "#f50057", borderWidth: "0px",borderRadius: "3px", fontSize: "0.875rem", padding: "8px 18px", color: "white"}} className="btn">Edit</button>
-                                                    <button onClick={this.delete} style={{backgroundColor: "dodgerblue", borderWidth: "0px",borderRadius: "3px", fontSize: "0.875rem", padding: "8px 18px", color: "white"}} className="btn">Delete</button>
-                                                </ButtonGroup>
-                                            </div>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </div>
-                </div>
-            </React.Fragment>
-        )
+    if (!value) {
+      alert("Please Enter a Task!");
+      return;
     }
+    if (arr.includes(value)) {
+      alert("Task already added to list!");
+      return;
+    } else {
+      setArr(
+        arr.map((val, ind) => {
+          if (ind === index) {
+            return value;
+          }
+
+          return val;
+        })
+      );
+    }
+  };
+
+  return (
+    <>
+      <h1
+        color="primary"
+        style={{
+          textAlign: "center",
+          background: "#f50057",
+          marginTop: "0px",
+          fontSize: "2.5rem",
+          padding: "0.1em",
+        }}
+      >
+        TODO APP
+      </h1>
+      <div className="main">
+        <div style={{ display: "inline-block" }}>
+          <TextField
+            id="standard-basic"
+            label="Enter Task"
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+          <button
+            onClick={addTask}
+            style={{
+              backgroundColor: "#f50057",
+              borderWidth: "0px",
+              borderRadius: "3px",
+              fontSize: "0.875rem",
+              padding: "8px 18px",
+              color: "white",
+              verticalAlign: "bottom",
+            }}
+            className="btn"
+          >
+            ADD
+          </button>
+          <ul
+            style={{
+              listStyleType: "square",
+              listStylePosition: "inside",
+              paddingLeft: "0px",
+              textAlign: "start",
+            }}
+          >
+            {arr.map((obj, index) => {
+              return (
+                <li key={index} style={{ marginBottom: "20px" }}>
+                  <h4 style={{ display: "inline-block" }}>{obj}</h4>
+                  <div style={{ display: "inline-block" }}>
+                    <ButtonGroup style={{ paddingLeft: "20px" }}>
+                      <button
+                        onClick={() => edit(index)}
+                        style={{
+                          backgroundColor: "#f50057",
+                          borderWidth: "0px",
+                          borderRadius: "3px",
+                          fontSize: "0.875rem",
+                          padding: "8px 18px",
+                          color: "white",
+                        }}
+                        className="btn"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        onClick={() => delete_(index)}
+                        style={{
+                          backgroundColor: "dodgerblue",
+                          borderWidth: "0px",
+                          borderRadius: "3px",
+                          fontSize: "0.875rem",
+                          padding: "8px 18px",
+                          color: "white",
+                        }}
+                        className="btn"
+                      >
+                        Delete
+                      </button>
+                    </ButtonGroup>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
